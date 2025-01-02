6ff58a0437ba1eae2ab5bc744556253f4420cd7f
tomal32232 <tomal42232665@gmail.com>
2025-01-02 18:08:56 +0600

    notification badge invisible

--- src/lib/components/NotificationIcon.svelte
+++ src/lib/components/NotificationIcon.svelte
@@ -13,7 +13,7 @@
   }
 </script>
 
-{#if true}
+{#if pendingCount > 0}
 <button class="notification-icon" on:click={handleClick} aria-label="Notifications">
   <Bell weight="fill" color="#E09453" size={24} />
   <span class="badge">{pendingCount}</span>
